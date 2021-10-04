25bc0eb0d9481aa559eda8de79c8d49507504dad
ArgentoKit <awisenone@gmail.com>
2021-10-04 22:26:21 +0300

    Create adaptive

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,9 @@ import 'bootstrap';
 import 'jquery';
 import '../scss/main.scss';
 import './flip-clock.js'
+
+$(function () {
+    $('.menu-btn').on('click', function() {
+        $('.nav').toggleClass('nav--open')
+    })
+})
